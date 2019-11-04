b22870dc92da90ee7613606b46c76612c33c332d
matheus-sexto <matheus.sexto@gmail.com>
2019-11-04 20:46:44 +0000

    Node.js and Express - 3.11 phonebook full stack

---
+++ build/precache-manifest.51c07f23663021de8ff750a5becbd4d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d717999c1b5c3b6806ae0a1da2eb5e19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0939b2bd433a6f7b375a",
+    "url": "/static/css/main.b5adea53.chunk.css"
+  },
+  {
+    "revision": "f1d5ee20e6359672ed92",
+    "url": "/static/js/2.e9d00b1f.chunk.js"
+  },
+  {
+    "revision": "0939b2bd433a6f7b375a",
+    "url": "/static/js/main.8d41ebeb.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
--- index.js
+++ index.js
@@ -15,6 +15,7 @@ morgan.token('body', function(req, res) {
 
 app.use(morgan(':method :status :res[content-length] - :response-time ms :body' ))
 
+app.use(express.static('build'))
 
 let persons = [
   {
